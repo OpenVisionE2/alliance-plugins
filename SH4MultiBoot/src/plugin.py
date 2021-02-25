18ffa0738c393aad0b9b00f6754271aaa03b55c3
persianpros <persianpros@yahoo.com>
2021-02-25 22:06:42 +0330

    Fix

--- SH4MultiBoot/src/plugin.py
+++ SH4MultiBoot/src/plugin.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-﻿from __future__ import print_function
+from __future__ import print_function
 from boxbranding import getImageDistro, getImageFileSystem
 from Screens.Screen import Screen
 from Screens.Console import Console
